8d257509f7591656199314e6b56e67368ea289fc
rumbleh <rumbleh@gmail.com>
2019-06-30 23:50:09 -0300

    Início do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:power_vendas_mobile/pages/dashboard.dart';
 
 void main() => runApp(PowerVendasMobile());
 
@@ -8,10 +9,6 @@ class PowerVendasMobile extends StatelessWidget {
     return MaterialApp(
         darkTheme: ThemeData.dark(),
         title: 'Power Vendas Mobile',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          primaryColorDark: Colors.redAccent,
-        ),
         home: PaginaPrincipal(
           title: 'Power Vendas Mobile',
         ));
@@ -28,55 +25,63 @@ class PaginaPrincipal extends StatefulWidget {
 }
 
 class _PaginaPrincipalState extends State<PaginaPrincipal> {
-  String tela = "";
+  Widget _tela = DashboardPage();
 
-  telaSelecionada(String valor) {
+  telaSelecionada(Widget tela) {
     setState(() {
-      tela = valor;
+      _tela = tela;
     });
-    return Text(tela);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Escolha"),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: Image(
-                      image: AssetImage( "profile.png"),
-                    ),
-                  )
-                ],
-              ),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                backgroundBlendMode: BlendMode.colorDodge,
-              ),
-            ),
-            ListTile(
-              title: Text('Dashboard'),
-              onTap: () {
-                telaSelecionada("DashBoard");
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Pedidos'),
-              onTap: () {
-                telaSelecionada("DashBoard");
-                Navigator.pop(context);
-              },
-            ),
-          ],
+        body: _tela,
+        appBar: AppBar(
+          title: Text("Power Vendas Mobile"),
+          elevation: 2.0,
         ),
-      ),
-    );
+        drawer: Drawer(
+            child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("José da Silva"),
+            accountEmail: Text("josesilva@powervendas.com.br"),
+            currentAccountPicture: new CircleAvatar(
+              backgroundImage: NetworkImage("https://i.pravatar.cc/150?img=18"),
+            ),
+          ),
+          ListTile(
+              title: Text("Dashboard"),
+              leading: Icon(Icons.dashboard),
+              onTap: () {
+                telaSelecionada(DashboardPage());
+                Navigator.pop(context);
+              }),
+          ListTile(
+            title: Text("Clientes"),
+            leading: Icon(Icons.person_pin),
+//            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+//              telaSelecionada("Clientes");
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text("Pedidos"),
+            leading: Icon(Icons.receipt),
+            onTap: () {
+//              telaSelecionada("Pedidos");
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text("Relatórios"),
+            leading: Icon(Icons.insert_chart),
+            onTap: () {
+//              telaSelecionada("Relatórios");
+              Navigator.pop(context);
+            },
+          )
+        ])));
   }
 }
