c5b5b7b15cb849a466d34b30e15efa406d00f671
rumbleh <rumbleh@gmail.com>
2019-07-03 06:44:14 -0300

    Início do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:power_vendas_mobile/pages/dashboard.dart';
+import 'package:power_vendas_mobile/pages/gerenciar_clientes.dart';
+import 'package:power_vendas_mobile/pages/pagina_principal.dart';
 
 void main() => runApp(PowerVendasMobile());
 
@@ -7,81 +8,14 @@ class PowerVendasMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        darkTheme: ThemeData.dark(),
-        title: 'Power Vendas Mobile',
-        home: PaginaPrincipal(
-          title: 'Power Vendas Mobile',
-        ));
+      darkTheme: ThemeData.dark(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => PaginaPrincipal(title: 'Power Vendas Mobile'),
+        '/clientes/listar': (context) => GerenciarClientes(title: 'Power Vendas Mobile'),
+      },
+      title: 'Power Vendas Mobile',
+    );
   }
 }
 
-class PaginaPrincipal extends StatefulWidget {
-  PaginaPrincipal({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _PaginaPrincipalState createState() => _PaginaPrincipalState();
-}
-
-class _PaginaPrincipalState extends State<PaginaPrincipal> {
-  Widget _tela = DashboardPage();
-
-  telaSelecionada(Widget tela) {
-    setState(() {
-      _tela = tela;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: _tela,
-        appBar: AppBar(
-          title: Text("Power Vendas Mobile"),
-          elevation: 2.0,
-        ),
-        drawer: Drawer(
-            child: ListView(padding: EdgeInsets.zero, children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text("José da Silva"),
-            accountEmail: Text("josesilva@powervendas.com.br"),
-            currentAccountPicture: new CircleAvatar(
-              backgroundImage: NetworkImage("https://i.pravatar.cc/150?img=18"),
-            ),
-          ),
-          ListTile(
-              title: Text("Dashboard"),
-              leading: Icon(Icons.dashboard),
-              onTap: () {
-                telaSelecionada(DashboardPage());
-                Navigator.pop(context);
-              }),
-          ListTile(
-            title: Text("Clientes"),
-            leading: Icon(Icons.person_pin),
-//            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-//              telaSelecionada("Clientes");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text("Pedidos"),
-            leading: Icon(Icons.receipt),
-            onTap: () {
-//              telaSelecionada("Pedidos");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text("Relatórios"),
-            leading: Icon(Icons.insert_chart),
-            onTap: () {
-//              telaSelecionada("Relatórios");
-              Navigator.pop(context);
-            },
-          )
-        ])));
-  }
-}
--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'gerenciar_clientes.dart';
+import 'gerenciar_pedidos.dart';
 
 class DashboardPage extends StatefulWidget {
   DashboardPage({Key key, this.title}) : super(key: key);
@@ -10,6 +12,58 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  int telaIndex = 0;
+  Widget telaAtiva;
+
+  final GerenciarClientes _gerenciarClientes = new GerenciarClientes();
+  final GerenciarPedidos _gerenciarPedidos = new GerenciarPedidos();
+
+  Widget pageChooser() {
+    switch (this.telaIndex) {
+      case 0:
+        return _gerenciarClientes;
+        break;
+
+      case 1:
+        return _gerenciarPedidos;
+        break;
+
+      default:
+        return GridView.count(
+          padding: EdgeInsets.all(10),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: <Widget>[
+            GridItem(Colors.indigo, Icons.person, "Clientes", "Gerenciar clientes", onTap: (){
+              telaSelecionada(0);
+              //Navigator.pushNamed(context, "/clientes/listar");
+            }),
+            GridItem(Colors.deepPurple, Icons.folder_special, "Produtos",
+                "Catálogo de produtos", onTap: (){
+                  telaSelecionada(1);
+                  //Navigator.pushNamed(context, "/clientes/listar");
+                }),
+
+
+            GridItem(Colors.red, Icons.receipt, "Pedidos", "Gerenciar pedidos"),
+            GridItem(Colors.green, Icons.chat, "Mensagens", "Enviar e receber"),
+            GridItem(Colors.amber, Icons.map, "Rotas", "Traçar rota de visita"),
+            GridItem(Colors.teal, Icons.picture_as_pdf, "Relatórios",
+                "Relatórios e gráficos"),
+            GridItem(Colors.lightGreen, Icons.monetization_on, "Lucros e perdas", "Movimentações financeiras"),
+            GridItem(Colors.grey, Icons.settings, "Configurações", "Ajustes do sistema"),
+          ],
+        );
+    }
+  }
+
+  void telaSelecionada(int index) {
+    setState(() {
+      this.telaIndex = index;
+    });
+  }
+
   Widget GridItem(
       MaterialColor cor, IconData icone, String texto, String detalhes,
       {Function() onTap}) {
@@ -63,25 +117,6 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      padding: EdgeInsets.all(10),
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      crossAxisCount: 2,
-      children: <Widget>[
-        GridItem(Colors.indigo, Icons.person, "Clientes", "Gerenciar clientes"),
-        GridItem(Colors.deepPurple, Icons.folder_special, "Produtos",
-            "Catálogo de produtos"),
-
-
-        GridItem(Colors.red, Icons.receipt, "Pedidos", "Gerenciar pedidos"),
-        GridItem(Colors.green, Icons.chat, "Mensagens", "Enviar e receber"),
-        GridItem(Colors.amber, Icons.map, "Rotas", "Traçar rota de visita"),
-        GridItem(Colors.teal, Icons.picture_as_pdf, "Relatórios",
-            "Relatórios e gráficos"),
-        GridItem(Colors.lightGreen, Icons.monetization_on, "Lucros e perdas", "Movimentações financeiras"),
-        GridItem(Colors.grey, Icons.settings, "Configurações", "Ajustes do sistema"),
-      ],
-    );
+    return pageChooser();
   }
 }
